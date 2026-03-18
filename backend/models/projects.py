6225cfefab5fc3cbaf1b44beb0085f049dc624eb
Harshit Singhal <thrivingharshit@gmail.com>
2026-03-18 21:46:35 +0530

    Add projects model.

--- backend/models/projects.py
+++ backend/models/projects.py
@@ -1,33 +1,50 @@
-from db.connection import get_db
+from db.connection import get_db_connection
 
-def get_projects():
-    conn = get_db()
+def get_all_projects():
+    conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
 
     cursor.execute("SELECT * FROM Project")
-    result = cursor.fetchall()
+    data = cursor.fetchall()
 
     cursor.close()
     conn.close()
+    return data
 
-    return result
 
-
-def create_project(data):
-    conn = get_db()
+def create_project(project_name, description, client_id, pm_id=None):
+    conn = get_db_connection()
     cursor = conn.cursor()
 
-    query = """
-    INSERT INTO Project (project_name, description, client_user_id)
-    VALUES (%s, %s, %s)
-    """
-
-    cursor.execute(query, (
-        data["project_name"],
-        data["description"],
-        data["client_user_id"]
-    ))
+    cursor.execute("""
+        INSERT INTO Project (project_name, description, client_user_id, project_manager_user_id)
+        VALUES (%s, %s, %s, %s)
+    """, (project_name, description, client_id, pm_id))
 
     conn.commit()
     cursor.close()
     conn.close()
+
+
+def get_projects_by_client(user_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM Project WHERE client_user_id=%s", (user_id,))
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return data
+
+
+def get_projects_by_pm(user_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM Project WHERE project_manager_user_id=%s", (user_id,))
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return data
