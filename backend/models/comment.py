bf60627440470a8f0a37dc13d89d880e628724e1
Harshit Singhal <thrivingharshit@gmail.com>
2026-03-18 21:51:36 +0530

    Add comment model.

--- backend/models/comment.py
+++ backend/models/comment.py
@@ -1,20 +1,32 @@
-from db.connection import get_db
+from db.connection import get_db_connection
 
-def add_comment_db(data):
-    conn = get_db()
+def add_comment(comment, file_id, user_id):
+    conn = get_db_connection()
     cursor = conn.cursor()
 
-    query = """
-    INSERT INTO Comment (comment_text, file_id, user_id)
-    VALUES (%s, %s, %s)
-    """
-
-    cursor.execute(query, (
-        data["comment"],
-        data["file_id"],
-        data["user_id"]
-    ))
+    cursor.execute("""
+        INSERT INTO Comment (comment_text, file_id, user_id)
+        VALUES (%s, %s, %s)
+    """, (comment, file_id, user_id))
 
     conn.commit()
     cursor.close()
     conn.close()
+
+
+def get_comments(file_id):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("""
+        SELECT c.comment_text, u.name
+        FROM Comment c
+        JOIN User u ON c.user_id = u.user_id
+        WHERE file_id=%s
+    """, (file_id,))
+
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return data
